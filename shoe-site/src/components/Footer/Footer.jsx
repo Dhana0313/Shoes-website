69e6f942737725ba112a7e441071676381e8e215
Dhananjaya <dananjayajayaweera1262@gmail.com>
2025-07-05 00:22:40 +0530

    Footer part

--- shoe-site/src/components/Footer/Footer.jsx
+++ shoe-site/src/components/Footer/Footer.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Footer() {
   return (
-    <></>
+    <footer className="bg-gray-800 text-white py-8 px-6 sm:px-8 lg:px-12 mt-12 rounded-t-xl">
+        <div className="container mx-auto text-center">
+          <p>&copy; {new Date().getFullYear()} ShoeStore. All rights reserved.</p>
+          <div className="flex justify-center space-x-4 mt-4">
+            <a href="#" className="text-gray-400 hover:text-white transition duration-300">Privacy Policy</a>
+            <a href="#" className="text-gray-400 hover:text-white transition duration-300">Terms of Service</a>
+          </div>
+        </div>
+      </footer>
   )
 }
 
